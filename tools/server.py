f16fc20b2be611409e18991158b6931e65d6cc70
Mehdi <mlauters@fly-n-sense.com>
2016-08-19 12:01:50 +0000

    Server fix

--- tools/server.py
+++ tools/server.py
@@ -1,6 +1,6 @@
 import socket
 import sys
-
+import time
 
 TEST="ABCDEFGHIJKLMNOPRSTUVWXYZ"
 
@@ -10,7 +10,10 @@ sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
 # Bind the socket to the address given on the command line
 server_name = sys.argv[1]
-test = sys.argv[2] == "test"
+try:
+  test = sys.argv[2] == "test"
+except:
+  test = False
 f = None
 if not test:
   f = open('/tmp/out.bin','w')
@@ -21,6 +24,7 @@ sock.bind(server_address)
 sock.listen(1)
 sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
 
+start = None
 while True:
   print >>sys.stderr, 'waiting for a connection'
   connection, client_address = sock.accept()
@@ -29,26 +33,41 @@ while True:
   try:
     if test:
       index = 0
+      cycles = 0
+      errors = 0
       while True:
         err = False
         data = connection.recv(9999)
-        for c in data:
-          needed = TEST[index%(len(TEST))]
-          if c != needed:
-            err = True
+        if data:
+          if start is None:
+            start = time.time();
+          for c in data:
+            if c == 'A':
+              cycles += 1
+            needed = TEST[index%(len(TEST))]
+            #print "%s %s"%(c,needed)
+            if c != needed:
+              err = True
+              break
+            index += 1
           if err:
-            print "%s != %s"%(c,needed)
+            #print "%d cycles"%cycles
+            #print "<%s> != <%s>"%(c,needed)
+            #print "<%s> != <%s>"%(c.encode('hex'),needed.encode('hex'))
             connection.sendall("-")
+            #print data
+            errors += 1         
           else:
             connection.sendall("+")
-          index += 1
-        print ">%s<"%data
+          end = time.time()
+          #if (index % 10) == 0:
+          t = end-start
+          print "%s byte %s seconds => %fB/s (%s %% errors)"%(index, t, (float(index) / float(t)), float(errors)/float(index))
     else:
       while True:
         data = connection.recv(9999)
         f.write(data)
         connection.sendall("+")
-        print >>sys.stderr, 'received "%s"' % data
 
   finally:
       if f is not None:
